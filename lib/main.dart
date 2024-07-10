c303ffa63fa1deb3853127f33ade4003bb43bbf5
LazizbekDev <lazizbektojiboyev0611@gmail.com>
2024-07-10 19:57:26 +0500

    my first app developing

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World By Dart!'),
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 85, 5, 117),
+          Color.fromARGB(255, 139, 39, 176)
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+        child:  const Center(
+          child: Text(
+            "Simpl Flutter",
+            style: TextStyle(
+                fontSize: 28, color: Colors.white, fontFamily: 'neue', fontWeight: FontWeight.w300,),
+          ),
         ),
       ),
-    );
-  }
+    ),
+  ));
 }
